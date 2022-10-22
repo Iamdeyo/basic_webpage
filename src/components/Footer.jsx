6de7ff282919623db4309e0ecbd84c1e7f422e36
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2022-10-22 08:22:53 +0100

    error page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as ZuriIcon } from '../assets/icons/zuri.svg';
 
 function Footer() {
   return (
-    <footer className="py-12 mb-7">
+    <footer className="py-12 px-4 mb-7">
       <div className="container mx-auto hidden items-center gap-8 pt-8 border-t border-gray-200 justify-between md:flex">
         <div>
           <ZuriIcon />
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Back } from '../assets/icons/backIcon.svg';
 
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="py-16 mt-2">
+      <p className="text-[#5925DC] text-base font-semibold mb-3">404 error</p>
+      <p className="font-semibold text-4xl mb-4 text-gray-900 md:mb-6 md:text-6xl">
+        We can't find that page
+      </p>
+      <p className="text-lg text-gray-600 font-normal mb-8 md:mb-12 md:text-xl">
+        {' '}
+        Sorry, the page you are looking for doesn't exist or has been moved
+      </p>
+      <div className="flex flex-col gap-3 md:flex-row">
+        <Link
+          to={'/'}
+          className="text-white text-center text-base font-semibold rounded-lg w-full py-3 px-5 gap-2 border bg-[#5925DC] md:w-fit md:order-2"
+        >
+          Take me home
+        </Link>
+        <Link
+          to={'/'}
+          className="text-gray-700 justify-center text-base font-semibold rounded-lg w-full py-3 px-5 gap-3 flex border border-[#D0D5DD] items-center md:w-fit md:order-1"
+        >
+          <Back /> Go back
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default ErrorPage;
