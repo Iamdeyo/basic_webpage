19a67196914cb8576940b19d953159e10f262e5f
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2022-10-27 15:57:51 +0100

    footer mobile styling updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import ErrorPage from './pages/ErrorPage';
 import Profile from './pages/Profile';
-import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -10,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<Profile />} />
-          <Route path="/contact" element={<Contact />} />
+
           <Route path="/*" element={<ErrorPage />} />
         </Routes>
         <Footer />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,19 @@ import { ReactComponent as ZuriIcon } from '../assets/icons/zuri.svg';
 
 function Footer() {
   return (
-    <footer className="py-12 px-4 mb-7">
-      <div className="container mx-auto hidden items-center pt-8 border-t border-gray-200 justify-between md:flex">
-        <div>
-          <ZuriIcon />
-        </div>
-        <p className="text-gray-500 text-base font-normal">
+    <footer className="py-8 px-4 md:py-12">
+      <div className="container mx-auto flex flex-col justify-center pt-8 border-t border-gray-200 gap-4 md:flex-row md:justify-between md:items-center">
+        <ZuriIcon className="h-6 w-fit md:h-8" />
+        <p className="text-gray-500 text-sm font-normal md:text-base">
           HNG Internship 9 Frontend Task
         </p>
         <div>
-          <img src={I4G} alt="" />
+          <img
+            src={I4G}
+            alt="I4G LOGO"
+            title="I4G LOGO"
+            className="h-6 md:h-8"
+          />
         </div>
       </div>
     </footer>
