cd0f5bbc77998e3ea2ef542778c9471c8de23beb
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2022-11-03 10:42:46 +0100

    feat: created contact page and added the routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
+import Contact from './pages/Contact';
 import ErrorPage from './pages/ErrorPage';
 import Profile from './pages/Profile';
 
@@ -9,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<Profile />} />
-
+          <Route path="/contact" exact element={<Contact />} />
           <Route path="/*" element={<ErrorPage />} />
         </Routes>
         <Footer />
