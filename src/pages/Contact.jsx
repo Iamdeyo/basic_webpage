58aff66b3c248c3cf3599bea08c7341cbb4f880d
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2022-11-04 07:39:14 +0100

    feat: Added content & style to the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,158 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Contact() {
-  return <div>Contact</div>;
+  const [agree, setAgree] = useState(false);
+  const [inputs, setInputs] = useState({});
+  const [errors, setErrors] = useState({ agreement: true, message: true });
+  const handleCheckbox = (e) => {
+    setAgree((prev) => (prev = !prev));
+  };
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    if (name === 'message') {
+      console.log(value.length);
+      value !== ''
+        ? setErrors((prev) => ({ ...prev, [name]: false }))
+        : setErrors((prev) => ({ ...prev, [name]: true }));
+    }
+    if (name === 'agreement') {
+      setErrors((prev) => ({ ...prev, [name]: !errors.agreement }));
+    }
+    setInputs((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
+
+  console.log(errors);
+
+  return (
+    <div className="pt-16 pb-8 px-4 container mx-auto md:pb-16 md:px-8">
+      {/* Header Section */}
+      <div className="mb-12 flex flex-col text-gray-900 gap-4 md:gap-5">
+        <p className="text-4xl font-semibold">Contact Me</p>
+        <p className="text-lg font-normal text-gray-600 md:text-xl">
+          {' '}
+          Hi there, contact me to ask me about anything you have in mind.
+        </p>
+      </div>
+      {/* Form Section */}
+      <form onSubmit={handleSubmit}>
+        <div id="form_fields" className="flex flex-col  gap-6">
+          <div className="flex flex-col items-start gap-[6px]">
+            <label
+              htmlFor="first_name"
+              className="text-sm font-medium text-gray-700"
+            >
+              Frist name
+            </label>
+            <input
+              id="first_name"
+              name="first_name"
+              type="text"
+              placeholder="Enter your first name"
+              className="input"
+              onChange={handleChange}
+              value={inputs.first_name || ''}
+            />
+            <span className="text-sm font-normal invisible text-[#F83F23]">
+              Please enter a message
+            </span>
+          </div>
+          <div className="flex flex-col items-start gap-[6px]">
+            <label
+              htmlFor="last_name"
+              className="text-sm font-medium text-gray-700"
+            >
+              Last name
+            </label>
+            <input
+              id="last_name"
+              name="last_name"
+              type="text"
+              placeholder="Enter your last name"
+              className="input"
+              onChange={handleChange}
+              value={inputs.last_name || ''}
+            />
+            <span className="text-sm font-normal invisible text-[#F83F23]">
+              Please enter a message
+            </span>
+          </div>
+          <div className="flex flex-col items-start gap-[6px]">
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="yourname@email.com"
+              className="input"
+              onChange={handleChange}
+              value={inputs.email || ''}
+            />
+            <span className="text-sm font-normal invisible text-[#F83F23]">
+              Please enter a message
+            </span>
+          </div>
+          <div className="flex flex-col items-start gap-[6px]">
+            <label
+              htmlFor="message"
+              className="text-sm font-medium text-gray-700"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Send me a message and I'll reply you as soon as possible..."
+              className="input min-h-[132px] resize-y focus:shadow-custom"
+              onChange={handleChange}
+              value={inputs.message || ''}
+            />
+            {errors.message ? (
+              <span className="text-sm font-normal  text-[#F83F23]">
+                Please enter a message
+              </span>
+            ) : (
+              ''
+            )}
+          </div>
+          <div className="flex gap-3">
+            <input
+              type="checkbox"
+              id="agreement"
+              name="agreement"
+              className={errors.agreement ? '' : 'focus disabled'}
+              onChange={handleChange}
+            />
+
+            <label
+              htmlFor="agreement"
+              className="text-base font-normal text-gray-600"
+            >
+              You agree to providing your data to Deyo who may contact you.
+            </label>
+          </div>
+        </div>
+        <button
+          id="btn__submit"
+          className="border border-blue-600 mt-8 rounded-lg w-full py-3 px-5 transition-all duration-300 text-white bg-blue-600 hover:bg-blue-700 hover:border-blue-700 disabled:bg-blue-200 disabled:border-blue-200 focus:shadow-custom"
+          disabled={Object.values(errors).includes(true)}
+        >
+          Send message
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: '0px 1px 2px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px #D1E9FF',
+      },
+    },
   },
   plugins: [],
 };
